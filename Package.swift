9fdc37634b4967bfe30e15daff8fd4209e6144db
CD Santiago <slashlos@gmail.com>
2021-02-15 18:04:29 -0500

    SPM init

--- Package.swift
+++ Package.swift
@@ -12,13 +12,15 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
 	    .library(name: "XMLDictionary", targets: ["XMLDictionary"]),
 		],
-        dependencies: [
-            // Dependencies declare other packages that this package depends on.
-        ],
-        targets: [
-            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-            // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-	    .target(name: "XMLDictionary", dependencies: ["XMLDictionary"]),
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+	    .target(name: "XMLDictionary", dependencies: [],
+				path: "XMLDictionary",
+				publicHeadersPath: "."),
     ]
 )
 
