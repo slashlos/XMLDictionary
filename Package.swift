d975fd1b12521746830d50facde53260a19336a7
cd santiago <slashlos@gmail.com>
2026-07-21 11:30:59 -0400

    update package metadata

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "XMLDictionary",
     platforms: [
-        .macOS, .iOS, .tvOS,
+        .macOS, .iOS
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
