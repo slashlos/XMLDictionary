66403654fb3f82a81d9295973b048dd291c2ab6a
cd santiago <slashlos@gmail.com>
2026-07-22 10:19:20 -0400

    exclude sub-project manifest

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 	    .target(name: "XMLDictionary", dependencies: [],
 				path: "XMLDictionary",
-				publicHeadersPath: "include"),
+                exclude: ["Package.swift", "Tests"], // Explicitly ignores the inner manifest
+				publicHeadersPath: "."),
     ]
 )
 
